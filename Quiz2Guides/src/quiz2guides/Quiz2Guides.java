b126d94929fdb42b9608a38091f80eed4598a140
Dario Arango <Dario Arango@DarioArango-PC>
2017-05-30 21:15:22 -0500

    Quiz 2 manejo de Guide

---
+++ Quiz2Guides/src/quiz2guides/Quiz2Guides.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz2guides;
+
+/**
+ *
+ * @author Dario Arango
+ */
+public class Quiz2Guides {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
